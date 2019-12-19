a1f206128d1dc493a35b10408b596bd2f646658e
Hssonger <58831051+Hssonger@users.noreply.github.com>
2019-12-18 18:40:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var client = new tmi.client(options);
 // Connect the client to the server
 client.connect();
 
-client.on('chat', function(channel, userstate, message, self){
+client.on('chat', function(channel, userstate, message, self) => {
 	var u = message.split('@');
 	if(u[1]){ // checking if someone is tagged
 		var name = u[1].substring(0, (process.env.USERNAME).length);
@@ -40,20 +40,24 @@ client.on('chat', function(channel, userstate, message, self){
 				
 if (message == "What is your name ? | What's your name ? |Who are you ?|What should I call you? |Do you have a name |I want to know you|Who are you?| Yours name?  | Tell yours name ") {
         client.say(userChannel, "Hi! My name is ai_hayden!")
+ }
+			
+if (message == "Where do you belong? | Where are you from | Where do you live") {
+client.say(userChannel, "If I tell you, I'll have to kill you. | That'll be a secret. :D") 
+ }
+			
+if (message == "ai_hayden") {
+client.say(useChannel, "best ai (ai_hayden) at your service.") 
+ }
+			
+if (message == "are you ai_hayden") {
+client.say(userChannel, "Yes, world's best ai (ai_hayden) to be exact.") 
+ }
+if (message == "are you working") {
+client.say(userChannel, "yes I am working perfectly (for now).") 
+	 }
 
-if (message == "Where do you belong? | Where are you from | Where do you live")
-client.say(userChannel, "If I tell you, I'll have to kill you. | That'll be a secret. :D")
-
-if (message == "ai_hayden")
-client.say(useChannel, "best ai (ai_hayden) at your service.")
-
-if (message == "are you ai_hayden")
-client.say(userChannel, "Yes, world's best ai (ai_hayden) to be exact.")
-
-if (message == "are you working")
-client.say(userChannel, "yes I am working perfectly (for now).")
-
-				
+		});				
 
 			{
 					timezoneOffset: '-300',
